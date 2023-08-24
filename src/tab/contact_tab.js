d07b699ad0c7e6376a621b7389abf7689aa7c74b
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-08-24 17:55:29 +0900

    feat: STOMP 소켓 연결완료 퍼블리싱중

--- src/tab/contact_tab.js
+++ src/tab/contact_tab.js
@@ -5,130 +5,188 @@
  * @format
  * @flow strict-local
  */
-import React from 'react';
-import {StyleSheet, View, Text, Button, ScrollView} from 'react-native'
-import { GestureHandlerRootView, TouchableOpacity } from 'react-native-gesture-handler';
+import { Client, Stomp } from '@stomp/stompjs';
+import React, { useEffect, useRef, useState } from 'react';
+import {StyleSheet, View, Text, Button, ScrollView, FlatList} from 'react-native'
+import { GestureHandlerRootView, State, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import * as encoding from 'text-encoding';
 
-let person = [
-    {
-        "name": "한상기",
-        "age": 15,
-        "phone": "010-6409-1048"
-    },
-    {
-        "name": "김지은",
-        "age": 20,
-        "phone": "01067330977"
-    },
-    {
-        "name": "May",
-        "age": 19,
-        "phone": "010-6409-1048"
-    },
-    {
-        "name": "Julia",
-        "age": 29,
-        "phone": "010-6409-1048"
-    },
-    {
-        "name": "Hunter",
-        "age": 18,
-        "phone": "010-6409-1048"
-    },
-    {
-        "name": "Jane",
-        "age": 28,
-        "gender": "010-6409-1048"
-    },
-    {
-        "name": "Kendrick",
-        "age": 34,
-        "phone": "010-6409-1048"
-    },
-    {
-        "name": "Kali",
-        "age": 25,
-        "phone": "010-6409-1048"
-    },
-    {
-        "name": "Jay",
-        "age": 37,
-        "phone": "010-6409-1048"
-    },
-    {
-        "name": "Mac",
-        "age": 31,
-        "phone": "010-6409-1048"
+const TabContactScreen = () => {
+
+    const [messages, setMessages] = useState([]);
+    const [textInput, setTextInput] = useState();
+
+    const clientRef = useRef(null);
+
+    useEffect(() => {
+        // STOMP 클라이언트 생성
+        clientRef.current = new Client({
+        brokerURL: 'http://218.155.95.66:8100/ws-stomp/websocket',
+        reconnectDelay: 5000,
+        forceBinaryWSFrames: true,
+        appendMissingNULLonIncoming: true,
+        debug: (msg) => {
+            console.log(msg)
+        },
+        onConnect: (frame) => {
+            console.log('Connected to STOMP server:', frame);
+            const subscription = clientRef.current.subscribe('/sub/chat/room/2', (message) => {
+                const newMessage = JSON.parse(message.body);
+                console.log(newMessage);
+                setMessages((prevMessages) => [...prevMessages, newMessage]);
+            });
+        },
+        onStompError: (error) => {
+            console.error('STOMP Error:', error);
+        },
+        onWebSocketError: (error) => {
+            console.error('WebSocket Error:', error);
+        },
+    });
+
+        // STOMP 연결 시작
+        clientRef.current.activate();
+
+        return () => {
+            if (clientRef.current) {
+                clientRef.current.deactivate();
+            }
+        };
+    }, []);
+
+    const onChangeInput = (event) => {
+        setTextInput(event);
     }
-];
 
-const press = (props, item) => {
-    props.navigation.navigate("ContactDetailScreen",{
-        name: item.name,
-        age: item.age,
-        phone: item.phone
-    })
-}
-
-const RenderPerson = (props) => {
-    return (
-        <GestureHandlerRootView>
-        {
-            person.map((item, idx) => (
-                <TouchableOpacity  
-                    style={styles.container} 
-                    key={idx}
-                    onPress={() => press(props, item)}
-                >
-                    <Text style={styles.name}>{item.name}</Text>
-                    <Text style={styles.age}>{item.age}</Text>
-                    <Text style={styles.gender}>
-                        {item.phone == null ? "번호없음" : item.phone}
-                    </Text>
-                </TouchableOpacity>
-            ))
+    const sendMessage = () => {
+        if (clientRef.current && clientRef.current.connected) {
+            const testMessage = { message: textInput, roomId:"2", sender: "Snagki", type:"TALK"};
+            clientRef.current.publish({ destination: "/pub/chat/sendMessage", body: JSON.stringify(testMessage) });
+            setTextInput("");
         }
-        </GestureHandlerRootView>
+    }
+  
+    return (
+        // <GestureHandlerRootView>
+        //     <ScrollView style={styles.tabContainer}>
+        //         {messages.map((message, index) => (
+        //         <View key={index} 
+        //             style={{ padding: 10}}>
+        //             <Text
+        //                 style={
+        //                     message.sender==="Snagki" ? {textAlign:'right'} : {textAlign: 'left'}
+        //                 }
+        //             >{message.sender}:  {message.message}</Text>
+        //         </View>
+        //         ))}
+        //     </ScrollView>
+        //     <View>
+        //         <TextInput
+        //             value={textInput}
+        //             onChangeText={onChangeInput}
+        //         />
+        //         <TouchableOpacity
+        //             onPress={sendMessage}
+        //             style={{ backgroundColor: 'lightblue', padding: 10, alignItems: 'center' }}
+        //         >
+        //             <Text>Send Message</Text>
+        //         </TouchableOpacity>
+        //     </View>
+        // </GestureHandlerRootView>
+        <View>
+            <View
+                style={{
+                    padding: 5,
+                    flexGrow: 1,
+                }}
+            >
+            <FlatList
+                style={styles.list}
+                contentContainerStyle={{paddingBottom: 50}}
+                data={serverMessages}
+                keyExtractor={(item, index) => item.message + index}
+                renderItem={({item}) =>
+                item.type == 'Welcome' || item.type == 'Leave' ? (
+                    <Text style={styles.welcomeChat}>{item.message}</Text>
+                ) : item.id == user.id ? (
+                    <MyChatCell chat={item}></MyChatCell>
+                ) : (
+                    <OtherChatCell chat={item}></OtherChatCell>
+                )
+                }
+            />
+            </View>
+            <View style={styles.bottomContainer}>
+            <TextInput
+                style={styles.input}
+                placeholder={'Add Message'}
+                onChangeText={text => {
+                setMessageText(text);
+                }}
+                value={messageText}></TextInput>
+            <Pressable onPress={sendMessage} disabled={messageText == ''}>
+                <Text style={styles.send}>Send</Text>
+            </Pressable>
+            </View>
+        </View>
     );
 };
- 
-class TabContactScreen extends React.Component{
-    render() {
-        return (
-            <ScrollView style={styles.tabContainer}>
-                <RenderPerson navigation={this.props.navigation}/>
-            </ScrollView>
-        ) 
-    }
-}
 
 const styles = StyleSheet.create({
-    tabContainer:{
-        marginTop:60
-    },
     container: {
-        borderRadius: 10,
-        borderColor: "black",
-        borderWidth: 1,
-        margin: 10,
+        flex: 1,
+        paddingTop: 30,
+        padding: 8,
+    },
+    welcomeChat: {
+        alignSelf: 'center',
         padding: 10,
-        color: "black",
-        backgroundColor: "white",
+        color: 'white',
+        backgroundColor: '#212124',
+        fontSize: 16,
+        borderRadius: 20,
+        marginVertical: 10,
     },
-    name: {
-        color: "black",
-        fontSize: 15,
-        fontWeight: "bold",
-        marginBottom: 5,
+    myChat: {
+        alignSelf: 'flex-end',
+        padding: 10,
+        color: 'white',
+        backgroundColor: 'yellow',
+        fontSize: 16,
+        borderRadius: 20,
+        marginVertical: 10,
     },
-    gender: {
-        color: "black",
-        fontSize: 15,
+    otherChat: {
+        alignSelf: 'flex-start',
+        padding: 10,
+        color: 'white',
+        backgroundColor: 'gray',
+        fontSize: 16,
+        borderRadius: 20,
+        marginVertical: 10,
     },
-    age: {
-        color: "black",
-        fontSize: 15,
+    input: {
+        width: '70%',
+        borderWidth: 1,
+        borderColor: 'black',
+        padding: 10,
+        marginRight: 30,
     },
-})
+    send: {
+        backgroundColor: 'black',
+        color: 'white',
+        padding: 20,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+    },
+    bottomContainer: {
+        marginHorizontal: 10,
+        flexDirection: 'row',
+        marginBottom: 80,
+    },
+    list: {
+        height: '80%',
+    },
+});
         
  export default TabContactScreen;
